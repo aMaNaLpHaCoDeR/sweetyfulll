ea74ad48c315f086448fa05d3246f26c39e4a29c
RoHaNaLpHaCoDeR <rohan.arora1812@gmail.com>
2025-05-30 13:59:52 +0530

    .

--- ReelDownloader.py
+++ ReelDownloader.py
@@ -1,3 +1,8 @@
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
